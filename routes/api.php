43deef333e5d90ad95a5f60c9f71e41ae85e04e2
puru9860 <uttam9808202718@gmail.com>
2020-02-07 16:11:31 +0545

    authentication api

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,40 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 //Route::resource('products','ProductsController');
 
 // List products
-Route::get('products','ProductsController@index');
-Route::get('product/create','ProductsController@create');
+Route::get('products', 'ProductsController@index');
+//Route::get('product/create', 'ProductsController@create');
 
 // Create new product
-Route::post('product','ProductsController@store');
+Route::post('product', 'ProductsController@store');
 
 // View single product
-Route::get('product/{id}','ProductsController@show');
+Route::get('product/{id}', 'ProductsController@show');
 
 // Update product
-Route::put('product/{id}','ProductsController@update');
-
+Route::put('product/{id}', 'ProductsController@update');
 
 // Delete product
-Route::delete('product/{id}','ProductsController@destroy');
+Route::delete('product/{id}', 'ProductsController@destroy');
+
+//Authentication routes
+//login route
+Route::post('login', 'AuthenticationController@login');
+
+//register route
+Route::post('register', 'AuthenticationController@register');
+
+//access control route
+Route::group(['middleware' => 'auth:api'], function ()
+{
+    Route::post('details', 'AuthenticationController@details');
+});
+
 
 
