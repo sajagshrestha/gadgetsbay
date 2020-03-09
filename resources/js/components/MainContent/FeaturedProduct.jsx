51ea125ba184aad0f77e490030209a1f8219d207
Sajag Shrestha <sajagshrestha0852@gmail.com>
2020-03-09 21:11:51 +0545

    my ad added

--- resources/js/components/MainContent/FeaturedProduct.jsx
+++ resources/js/components/MainContent/FeaturedProduct.jsx
@@ -3,20 +3,23 @@ import "./FeaturedProduct.css";
 import HomeAdView from "../Layouts/HomeAdView";
 
 const FeaturedProduct = ({ url }) => {
-	const [products, setProduct] = useState([]);
-	useEffect(() => {
-		axios.get(`/api/${url}`).then(response => {
-			setProduct(response.data.data);
-		});
-	}, []);
+    const [products, setProduct] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`/api/${url}`)
+            .then(response => {
+                setProduct(response.data.data);
+            })
+            .catch(error => console.log(error));
+    }, [products.length]);
 
-	return (
-		<div className="featured-products">
-			{products.map(product => (
-				<HomeAdView product={product} key={product.id} />
-			))}
-		</div>
-	);
+    return (
+        <div className="featured-products">
+            {products.map(product => (
+                <HomeAdView product={product} key={product.id} />
+            ))}
+        </div>
+    );
 };
 
 export default FeaturedProduct;
--- resources/js/components/MainContent/MyAds.jsx
+++ resources/js/components/MainContent/MyAds.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../App";
+import axios from "axios";
 
 const MyAds = () => {
-    return <div>my Ads</div>;
+    const [Ad, setAd] = useState({});
+    const { globalToken } = useContext(UserContext);
+    useEffect(() => {
+        axios
+            .get("/api/user/products", globalToken)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+    });
+    return <div>my ads</div>;
 };
 export default MyAds;
